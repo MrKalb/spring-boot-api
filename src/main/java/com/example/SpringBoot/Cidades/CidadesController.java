3bab08d150befe46e4b6a75bca0b8c391cf84732
MrKalb <ikalb9@gmail.com>
2019-02-13 17:51:40 -0200

    Changes

--- src/main/java/com/example/SpringBoot/Cidades/CidadesController.java
+++ src/main/java/com/example/SpringBoot/Cidades/CidadesController.java
@@ -6,8 +6,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
-public class CidadesController {
+@Service
+public class CidadesService {
 
 	@Autowired
 	private CidadesProperty cidadeProperty;
